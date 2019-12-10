d805f367f6126abf9248b7611aa8af1238dbf86e
Majid Khorsandi <majid.khorsandi@tink.se>
2019-12-10 06:49:11 +0100

    feat: Add sorting

--- src/main/java/com/github/majidkhorsandi/Main.java
+++ src/main/java/com/github/majidkhorsandi/Main.java
@@ -1,15 +1,13 @@
 package com.github.majidkhorsandi;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         FileReader fileReader;
         try {
-             fileReader = new FileReader(new File("/Users/majid/code/word-count/src/main/resources/words.txt"));
+            fileReader = new FileReader(new File("/Users/majid/code/word-count/src/main/resources/words.txt"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             return;
@@ -18,7 +16,7 @@ public class Main {
 
         String line = bufferedReader.readLine();
         String[] wordsInLine = line.split("\\s");
-        Map<String, Integer> wordCounts = new HashMap<String, Integer>();
+        Map<String, Integer> wordCounts = new HashMap<>();
         Arrays.stream(wordsInLine).forEach(word -> {
             if (wordCounts.containsKey(word)) {
                 wordCounts.put(word, wordCounts.get(word) + 1);
@@ -26,6 +24,22 @@ public class Main {
                 wordCounts.put(word, 1);
             }
         });
-        wordCounts.forEach((w, c)-> System.out.println(w + ": " + c));
+
+        List<Map.Entry<String, Integer>> sorted = sort(wordCounts);
+        sorted.forEach(e -> System.out.println(e.getKey() + ": " + e.getValue()));
+    }
+
+    private static List<Map.Entry<String, Integer>> sort(Map<String, Integer> wordCount) {
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(wordCount.entrySet());
+        entries.sort((o1, o2) -> {
+            if (o1.getValue().equals(o2.getValue())) {
+                return 0;
+            } else if (o1.getValue() < o2.getValue()) {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+        return entries;
     }
 }
