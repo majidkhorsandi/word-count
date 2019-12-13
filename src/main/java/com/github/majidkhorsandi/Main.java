5b3ab4fc850b075fb49b69809c1e662fcf27ba9a
Majid Khorsandi <majid.khorsandi@tink.se>
2019-12-13 22:19:24 +0100

    feat: Add timer

--- src/main/java/com/github/majidkhorsandi/Main.java
+++ src/main/java/com/github/majidkhorsandi/Main.java
@@ -1,12 +1,16 @@
 package com.github.majidkhorsandi;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         Counter counter = new Counter();
         Map<String, Integer> wordsInFile;
+        LocalDateTime start = LocalDateTime.now();
         try {
             wordsInFile = counter.countWordsInFile("/Users/majid/code/word-count/src/main/resources/words.txt");
         } catch (IOException e) {
@@ -14,7 +18,9 @@ public class Main {
             return;
         }
         List<Map.Entry<String, Integer>> sorted = Sorter.sort(wordsInFile);
+        Duration duration = Duration.between(start, LocalDateTime.now());
         sorted.forEach(e -> System.out.println(e.getKey() + ": " + e.getValue()));
+        System.out.printf("execution time: %s ms", duration.toMillis());
     }
 
 }
